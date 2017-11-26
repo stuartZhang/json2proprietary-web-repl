4540c5853caaf233ee02088fbc41485192594291
stuartZhang <stuartpage@qq.com>
2017-11-26 23:52:34 +0800

    Finish the basic features.

--- public/javascripts/repl.mjs
+++ public/javascripts/repl.mjs
@@ -1,3 +1,22 @@
 const params = [9, 2];
 const [log] = params;
 console.log('test', log);
+
+const btnSend = document.querySelector('form[name=query] button[name=send]');
+const txtIden = document.querySelector('form[name=query] textarea[name=iden]');
+const txtPayload = document.querySelector('form[name=query] textarea[name=payload]');
+const txtRes = document.querySelector('textarea#res');
+btnSend.addEventListener('click', async () => {
+  const res = await fetch('/lbs-api-repl', {
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    }),
+    body: JSON.stringify({
+      iden: JSON.parse(txtIden.value),
+      payload: JSON.parse(txtPayload.value)
+    })
+  }).then(res => res.json());
+  txtRes.value = JSON.stringify(res, null, 2);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const tpsConnect = require('lbs-connect-driver');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.route('/lbs-api-repl').get((req, res) => {
+  res.render('lbs_api_repl');
+}).post(async (req, res, next) => {
+  try {
+    if (req.body.iden && req.body.payload) {
+      const response = await tpsConnect(req.body.iden, req.body.payload);
+      res.json(response);
+    } else {
+      throw new Error(`Miss parameter: ${JSON.stringify(req.body, null, 2)}`);
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
