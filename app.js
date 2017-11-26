a53162976c3140e917647a15e9d2785ec7b2fa98
stuartZhang <stuartpage@qq.com>
2017-11-26 12:39:01 +0800

    Utilize the browserify to dynamically compile the ES 6 js to ES 5.

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const sassMiddleware = require('node-sass-middleware');
+const babelifyMiddle = require('express-babelify-middleware');
+const fs = require('fs');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,6 +28,11 @@ app.use(sassMiddleware({
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
+// See also: https://github.com/luisfarzati/express-babelify-middleware
+app.get('/javascripts/repl.js', babelifyMiddle('public/javascripts/repl.bjs', {
+  basedir: path.join(__dirname, 'public/javascripts'),
+  grep: /\.bjs$/
+}, JSON.parse(fs.readFileSync('./.babelrc'))));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -33,7 +40,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
