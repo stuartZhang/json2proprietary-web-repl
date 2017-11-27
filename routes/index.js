062e637641c6bc3b8a27667702b10b6bc7c75d9c
stuartZhang <stuartpage@qq.com>
2017-11-27 23:10:59 +0800

    build a route-handle wrapper to  trap any kind of errors and feedback 500 error to the client.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const {render} = require('../utils');
+const {render, safe} = require('../utils');
 const tpsConnect = require('lbs-connect-driver');
 const router = module.exports = express.Router();
 /* GET home page. */
 router.get('/', render('index', {title: 'Express'}));
-router.route('/lbs-api-repl').get(render('lbs_api_repl')).post(async (req, res, next) => {
-  try {
-    if (req.body.iden && req.body.payload) {
-      const response = await tpsConnect(req.body.iden, req.body.payload);
-      res.json(response);
-    } else {
-      throw new Error(`Miss parameter: ${JSON.stringify(req.body, null, 2)}`);
-    }
-  } catch (err) {
-    next(err);
+router.route('/lbs-api-repl').get(render('lbs_api_repl')).post(safe(async (req, res) => {
+  const {iden, payload} = req.body;
+  if (iden && payload) {
+    const response = await tpsConnect(iden, payload);
+    res.json(response);
+  } else {
+    throw new Error(`Miss parameter: ${JSON.stringify(req.body, null, 2)}`);
   }
-});
+}));
--- utils/index.js
+++ utils/index.js
@@ -5,5 +5,14 @@ _.extendOwn(exports, {
     return (req, res) => {
       res.render(layout, args);
     };
+  },
+  safe(func){
+    return async (req, res, next) => {
+      try {
+        await func(req, res, next);
+      } catch (err) {
+        next(err);
+      }
+    };
   }
 });
