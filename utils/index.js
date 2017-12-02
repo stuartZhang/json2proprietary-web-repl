75a7c8093ecb1c83b4b0f7d26ff8b4da7fd9d2fd
stuartZhang <stuartpage@qq.com>
2017-12-02 14:30:33 +0800

    dynamic read the local ip address and import the correct https certs

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,6 @@
 const _ = require('underscore');
+const os = require('os');
+const debug = require('debug');
 
 _.extendOwn(exports, {
   render(layout, args){
@@ -14,5 +16,25 @@ _.extendOwn(exports, {
         next(err);
       }
     };
+  },
+  getLocalIps(){
+    const log = debug('json2proprietary-web-repl:Local-IP');
+    const ips = [];
+    for (const [ifname, ifaces] of Object.entries(os.networkInterfaces())) {
+      let alias = 0;
+      for (const iface of ifaces) {
+        if ('IPv4' !== iface.family || iface.internal !== false) { // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+          continue;
+        }
+        ips.push(iface.address);
+        if (alias >= 1) { // this single interface has multiple ipv4 addresses
+          log(`${ifname}:${alias} ${iface.address}`);
+        } else { // this interface has only one ipv4 adress
+          log(`${ifname} ${iface.address}`);
+        }
+        ++alias;
+      }
+    }
+    return ips.filter(ip => _.isString(ip) && !_.isEmpty(ip));
   }
 });
