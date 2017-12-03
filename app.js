f4e63cc9989adbbf4071fedf2034c0b1c99b6b10
stuartZhang <stuartpage@qq.com>
2017-12-03 12:06:59 +0800

    format the code

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const tmplEngine = require('./utils/template-engine');
 const index = require('./routes/index');
 const users = require('./routes/users');
 
-const app = express();
+const app = module.exports = express();
 // view engine setup
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -44,10 +44,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(_.extendOwn(new Error('Not Found'), {status: 404})));
-
 // error handler
 app.use(function(err, req, res, next) { // render the error page
   res.status(err.status || 500);
@@ -56,5 +54,3 @@ app.use(function(err, req, res, next) { // render the error page
     error: req.app.get('env') === 'development' ? err : {}
   });
 });
-
-module.exports = app;
