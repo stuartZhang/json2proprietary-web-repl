6f9545be6205bc745f3ba6fec45eb39394e877f1
Stuart (Haoyu)Zhang <stzhang@tjnimone.com>
2017-12-07 10:06:17 +0800

    Disable the eslint for the generated js file.

--- utils/mjs-transpile-middleware.js
+++ utils/mjs-transpile-middleware.js
@@ -3,12 +3,13 @@ const rollupMiddleware = require('express-middleware-rollup');
 module.exports = function mjsTranspiler(cliArgs, root, app){
   app.use('/javascripts', rollupMiddleware({
     root,
-    src: 'public',
-    destExtension: /-es5\.js$/,
-    bundleExtension: '.mjs',
-    // rebuild: 'always',
-    bundleOpts: {
-      sourceMap: cliArgs.isDebug ? 'inline' : false
+    'src': 'public',
+    'destExtension': /-es5\.js$/,
+    'bundleExtension': '.mjs',
+    // 'rebuild': 'always',
+    'bundleOpts': {
+      'banner': '/* eslint-disable */',
+      'sourceMap': cliArgs.isDebug ? 'inline' : false
     }
   }));
 };
