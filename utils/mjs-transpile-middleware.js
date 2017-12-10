a82e97e4cfc94917ec170e016154749d191faa62
stuartZhang <stuartpage@qq.com>
2017-12-10 22:39:26 +0800

    Import the custom rollup-babel plugin

--- utils/mjs-transpile-middleware.js
+++ utils/mjs-transpile-middleware.js
@@ -1,4 +1,5 @@
 const rollupMiddleware = require('express-middleware-rollup');
+const babelPlugin = require('express-middleware-rollup/rollup-plugin-babel');
 
 module.exports = function mjsTranspiler(cliArgs, root, app){
   app.use('/javascripts', rollupMiddleware({
@@ -7,6 +8,13 @@ module.exports = function mjsTranspiler(cliArgs, root, app){
     'destExtension': /-es5\.js$/,
     'bundleExtension': '.mjs',
     // 'rebuild': 'always',
+    'rollupOpts': {
+      'external': ['jsoneditor'],
+      'plugins': [babelPlugin({ // .babelrc 自动装载
+        'externalHelpers': true,
+        'exclude': 'node_modules/**'
+      })]
+    },
     'bundleOpts': {
       'globals': {
         'jsoneditor': 'JSONEditor'
